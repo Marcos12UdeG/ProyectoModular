4f5ee57754091889ad3f5dd32ab73324fcdc971d
Doomsday2409 <pacoArrezola@gmail.com>
2025-10-06 13:37:48 -0600

    fix: cambio de endpoint a HTTPS para login

--- frontend/proyecto-modular/src/app/page.tsx
+++ frontend/proyecto-modular/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8000/login", {
+      const res = await fetch("https://storytellermodular.lat/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
